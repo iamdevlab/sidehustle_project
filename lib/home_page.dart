1b1beafd1c889d45ec4ec1035c8b18bf565a0735
iamdevlab <primedcoderz@gmail.com>
2021-11-10 21:42:09 +0100

    proto

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:sd_project/tile_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,136 +8,131 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange[100],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue[100],
+      appBar: AppBar(
+        title: Text("My Profile"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[300]!),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Container(
+                          height: 80,
+                          width: 70,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.green),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Group 15",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "group15@intership.ng",
+                                style: TextStyle(),
+                              ),
+                            ),
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Side Hustle 4.0 internship",
+                                style: TextStyle(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            child: Icon(
-              EvaIcons.person,
-              size: 100,
-              color: Colors.blue[850],
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "General",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "GROUP 15",
-              style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.blue[800],
-                  fontWeight: FontWeight.bold),
+            InfoTile(
+              leading_icon: Icon(
+                EvaIcons.person,
+                color: Colors.blue[800],
+              ),
+              title: Text("Edit Profile"),
+              trailing_icon: Icon(EvaIcons.arrowIosForward),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                  EvaIcons.email,
-                  color: Colors.red[800],
-                ),
+            InfoTile(
+              leading_icon: Icon(
+                EvaIcons.briefcase,
+                color: Colors.blue[800],
               ),
-              Text(
-                "group15@internship.com",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.blue[800],
-                ),
+              title: Text("Badges"),
+              trailing_icon: Icon(EvaIcons.arrowIosForward),
+            ),
+            InfoTile(
+              leading_icon: Icon(
+                Icons.add_circle_outline,
+                color: Colors.blue[800],
               ),
-            ],
-          ),
-          // SizedBox(
-          //     //height: 500,
-          //     ),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Connect with us on: "),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                  EvaIcons.facebook,
-                  color: Colors.blue[800],
-                ),
+              title: Text("Study Goals"),
+              trailing_icon: Icon(EvaIcons.arrowIosForward),
+            ),
+            InfoTile(
+              leading_icon: Icon(
+                Icons.notifications_off,
+                color: Colors.blue[800],
               ),
-              Text(
-                "facebook.com/group15",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.blue[800],
-                ),
+              title: Text("Focus Mode"),
+              trailing_icon: Icon(EvaIcons.arrowIosForward),
+            ),
+            InfoTile(
+              leading_icon: Icon(
+                Icons.redo_rounded,
+                color: Colors.blue[800],
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                  EvaIcons.twitter,
-                  color: Colors.blue[400],
-                ),
+              title: Text("School Schedule"),
+              trailing_icon: Icon(EvaIcons.arrowIosForward),
+            ),
+            InfoTile(
+              leading_icon: Icon(
+                Icons.person_search_outlined,
+                color: Colors.blue[800],
               ),
-              Text(
-                "@group15",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.blue[800],
-                ),
-              ),
-            ],
-          ),
-          // ListTile(
-          //   leading: Icon(EvaIcons.email),
-          //   title: Text(
-          //     "primedcoderz@gmail.com",
-          //     style: TextStyle(
-          //       fontSize: 18,
-          //       color: Colors.blue[800],
-          //     ),
-          //   ),
-          // ),
-          // ListTile(
-          //   leading: Icon(
-          //     EvaIcons.facebook,
-          //     color: Colors.blue[800],
-          //   ),
-          //   title: Text(
-          //     "facebook.com/iamdevlab",
-          //     style: TextStyle(
-          //       fontSize: 18,
-          //       color: Colors.blue[800],
-          //     ),
-          //   ),
-          // ),
-          // ListTile(
-          //   leading: Icon(
-          //     EvaIcons.twitter,
-          //     color: Colors.blue[400],
-          //   ),
-          //   title: Text(
-          //     "@grimconscript",
-          //     style: TextStyle(
-          //       fontSize: 18,
-          //       color: Colors.blue[800],
-          //     ),
-          //   ),
-          // ),
-        ],
+              title: Text("Invite Friends"),
+              trailing_icon: Icon(EvaIcons.arrowIosForward),
+            ),
+          ],
+        ),
       ),
     );
   }
